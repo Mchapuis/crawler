c12afb661c827b882ebf9857666cd143de51525f
Martine Chapuis <machapu@gmail.com>
2018-11-29 01:42:29 -0500

    update json -- trying to get bigger sample

--- scrapyCrawler/scrapyCrawler/spiders/quotes_spider.py
+++ scrapyCrawler/scrapyCrawler/spiders/quotes_spider.py
@@ -19,7 +19,7 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = ['https://www.concordia.ca/about.html']
     list_domains = ['explore.concordia.ca', 'twitter.com', 'facebook.com', 'youtube.com', 'flickr.com','legisquebec.gouv.qc.ca']
-    rules = [Rule(LinkExtractor(allow=r'concordia.ca', deny=r'explore.concordia.ca', deny_extensions=r'pdf'), callback='parse', follow=True)]
+    rules = [Rule(LinkExtractor(allow=r'concordia.ca', deny=r'explore.concordia.ca'), callback='parse', follow=True)]
     allowed_domains = ["concordia.ca"]#concordia.ca/(.*?).html"]
     #deny_domains = ["explore.concordia.ca"]
     #deny_extensions = ["pdf"]
@@ -30,30 +30,35 @@ class QuotesSpider(scrapy.Spider):
         
 
     def parse(self, response):
-        title = response.css('title::text').extract_first()
+        
         extractor = Extractor(extractor='ArticleExtractor', html=response.text)
-        #div = response.xpath('//div[@class="d-grid-main"]')
-        #res = div.xpath('.//p[@class="class-name"]/text()').extract()
-        #res = div.xpath('normalize-space(.//p[@class="class-name"])').extract()
-        #my_res = response.xpath('normalize-space(.//p[@class="class-name"])').extract()
 
-        #soup = BeautifulSoup(extractor.getHTML(), 'html.parser')
-
-        #res = soup.get_text()
-        if "Explore" in title:
-            return
-        try:
-            my_text = extractor.getText()
-        except scrapy.exceptions.NotSupported:
-            return
+        current_page = response.url
+        extension = str(current_page)[-4:]
+        
         afinn = Afinn()
-        scored_text = afinn.score(my_text)
-        yield ScrapycrawlerItem(title=title,
+        if extension == "html":
+            title = response.css('title::text').extract_first()
+
+            if "Explore" in title:
+                return
+            my_text = extractor.getText()
+            scored_text = afinn.score(my_text)
+            yield ScrapycrawlerItem(title=title,
                         text=my_text,
                         url=response.url,
                         field=self.name,
                         sentiment=scored_text)
-        self.links.append(response.url)
-
-        for href in response.css('a::attr(href)'):
-            yield response.follow(href, self.parse)
+                
+            for href in response.css('a::attr(href)').extract():
+                if ".pdf" not in href:
+                    yield scrapy.Request(response.urljoin(href),
+                                            callback=self.parse)
+                
+                #urls = le.extract_links(response)
+"""         for href in response.css('a::attr(href)'):
+            _url = str(href)
+            _url = href('data')
+            print("_url is "+_url.data)
+            if href.data[-4:] == "html":
+                yield response.follow(href, self.parse) """
